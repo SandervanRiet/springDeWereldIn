57875e4f4e0823a9319516db70faa94dc763654a
SandervanRiet <r0903044@student.thomasore.be>
2022-10-26 14:27:45 +0200

    specialnumber in controller

--- src/main/java/com/example/springdewereldin/HomeController.java
+++ src/main/java/com/example/springdewereldin/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.springdewereldin;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @Value("${specialnumber:1111}")
     private int specialNumber;
 
     @GetMapping("/")
