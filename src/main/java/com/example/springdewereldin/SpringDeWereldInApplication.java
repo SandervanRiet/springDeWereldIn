8e2c7040a6db543cbfeee872d4a8709e3e7d3966
SandervanRiet <r0903044@student.thomasore.be>
2022-10-26 10:03:12 +0200

    begin

---
+++ src/main/java/com/example/springdewereldin/SpringDeWereldInApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdewereldin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeWereldInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDeWereldInApplication.class, args);
+    }
+
+}
